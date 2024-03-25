a1e35273a7c2e4c94b43255338e7fa2040e71ebc
Sona-2005 <162302636+Sona-2005@users.noreply.github.com>
2024-03-25 14:48:44 +0400

    Create CommunityChestTile.java

---
+++ CommunityChestTile.java
@@ -0,0 +1,9 @@
+public class CommunityChestTile extends Tile{
+  
+    public CommunityChestTile(int position){
+        super(position);
+    }
+    public void takeCommunityChestCard(){
+
+    }
+}
