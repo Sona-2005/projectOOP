6de39699d52f145f06758917d021c699de31d586
Sona-2005 <162302636+Sona-2005@users.noreply.github.com>
2024-03-25 14:46:13 +0400

    Create Board.java

---
+++ Board.java
@@ -0,0 +1,15 @@
+public class Board {
+    private Tile[] board;
+
+    public Board() {
+
+    }
+
+//    public Tile[] getBoard() {
+//        Tile[] newArr =  new Tile[board.length];
+//        for (int i = 0; i < board.length; i++) {
+//            newArr[i] = new Tile(i);
+//
+//        }
+//    }
+}
