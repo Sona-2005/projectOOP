6e08387794165a887f73fa0d723ebafc31f302ad
Sona-2005 <162302636+Sona-2005@users.noreply.github.com>
2024-03-25 14:49:19 +0400

    Create Dice.java

---
+++ Dice.java
@@ -0,0 +1,7 @@
+public class Dice {
+    public static int[] rollDice() {
+        int a = (int) (Math.random()*6 + 1);
+        int b = (int) (Math.random()*6 + 1);
+        return new int[]{a, b};
+    }
+}
