d6155c4e3dbf2090c99278dd17b438a18fb59f04
Sona-2005 <162302636+Sona-2005@users.noreply.github.com>
2024-05-10 22:59:42 +0400

    Update and rename Card.java to monopoly/core/Card.java

--- monopoly/core/Card.java
+++ monopoly/core/Card.java
@@ -1,3 +1,5 @@
+package monopoly.core;
+
 public enum Card{
 
     //chance cards
@@ -5,17 +7,14 @@ public enum Card{
     CARD2("Advance to Go (Collect $200)", Keyword.MOVE_TO, 0),
     CARD3("Advance to Illinois Avenue. If you pass Go, collect $200", Keyword.MOVE_TO, 24),
     CARD4("Advance to St. Charles Place. If you pass Go, collect $200", Keyword.MOVE_TO, 11),
-    CARD5("Advance to the nearest Railroad. If unowned, you may buy it from the Bank. If owned, pay wonder twice the rental to which they are otherwise entitled",
-            Keyword.RAILROAD, 0),
-    CARD6("Advance to the nearest Railroad. If unowned, you may buy it from the Bank. If owned, pay wonder twice the rental to which they are otherwise entitled",
-            Keyword.RAILROAD, 0),
-    CARD7("Advance token to nearest Utility. If unowned, you may buy it from the Bank. If owned, throw dice and pay owner a total ten times amount thrown.",
-            Keyword.RAILROAD, 0),
+    CARD5("Advance to the B&O Railroad. If you pass Go, collect $200", Keyword.MOVE_TO, 25),
+    CARD6("Advance to the Pennsylvania Railroad. If you pass Go, collect $200", Keyword.MOVE_TO, 15),
+    CARD7("Advance token to Electric Company. If you pass Go, collect $200", Keyword.MOVE_TO, 12),
     CARD8("Bank pays you dividend of $50", Keyword.CHANGE_MONEY, 50),
     CARD9("Get Out of Jail Free", Keyword.JAIL_FREE, 0),
     CARD10("Go Back 3 Spaces", Keyword.MOVE, -3),
     CARD11("Go to Jail. Go directly to Jail, do not pass Go, do not collect $200", Keyword.JAIL, 0),
-    CARD12("Make general repairs on all your property. For each house pay $25. For each hotel pay $100", Keyword.PAY_FOR_HOUSES, -25),
+    CARD12("Make general repairs on all your property.Pay $25", Keyword.CHANGE_MONEY, -25),
     CARD13("Speeding fine $15", Keyword.CHANGE_MONEY, -15),
     CARD14("Take a trip to Reading Railroad. If you pass Go, collect $200", Keyword.MOVE_TO, 0),
     CARD15("You have been elected Chairman of the Board. Pay each player $50", Keyword.CHANGE_MONEY_EACH, -50),
@@ -35,12 +34,12 @@ public enum Card{
     CARD27("Pay hospital fees of $100", Keyword.CHANGE_MONEY, -100),
     CARD28("Pay school fees of $50", Keyword.CHANGE_MONEY_EACH, -50),
     CARD29("Receive $25 consultancy fee", Keyword.CHANGE_MONEY, 25),
-    CARD30("You are assessed for street repair. $40 per house. $115 per hotel", Keyword.PAY_FOR_HOUSES, -40),
+    CARD30("You are assessed for street repair. Pay $40.", Keyword.CHANGE_MONEY, -40),
     CARD31("You have won second prize in a beauty contest. Collect $10", Keyword.CHANGE_MONEY, 10),
     CARD32("You inherit $100", Keyword.CHANGE_MONEY, 100);
 
     public enum Keyword{
-        JAIL, JAIL_FREE, CHANGE_MONEY, CHANGE_MONEY_EACH, PAY_FOR_HOUSES, MOVE, MOVE_TO, RAILROAD;  //to think about railroad
+        JAIL, JAIL_FREE, CHANGE_MONEY, CHANGE_MONEY_EACH, MOVE, MOVE_TO;  //to think about railroad
     }
 
     private String message;
@@ -62,10 +61,13 @@ public enum Card{
         switch (keyword) {
             case Keyword.JAIL:
                 p.goToJail(b);
+                break;
             case Keyword.JAIL_FREE:
                 p.addHasJailFreeCard();
+                break;
             case Keyword.CHANGE_MONEY:
                 p.changeMoney(value);
+                break;
             case Keyword.CHANGE_MONEY_EACH:
                 Player[] otherPlayers = b.getPlayers();
                 p.changeMoney(otherPlayers.length * value);
@@ -75,15 +77,13 @@ public enum Card{
                     }
                     player.changeMoney(-value);
                 }
-            case Keyword.PAY_FOR_HOUSES:
-                p.changeMoney(value);
+                break;
             case Keyword.MOVE:
                 p.moveSpaces(value);
+                break;
             case Keyword.MOVE_TO:
                 p.moveTo(value);
-            case Keyword.RAILROAD:
-                p.moveTo(value); // to change
+                break;
         }
     }
-
 }
